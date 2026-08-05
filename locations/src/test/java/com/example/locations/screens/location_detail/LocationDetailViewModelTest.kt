a15ba468ad4f2d38143f2c2928f2c2e2303a09e5
Orlandroid <lopez.orlando.gold9@gmail.com>
2026-08-05 16:33:07 -0600

    Moved tests to his specific modules

---
+++ locations/src/test/java/com/example/locations/screens/location_detail/LocationDetailViewModelTest.kt
@@ -0,0 +1,71 @@
+package com.example.locations.screens.location_detail
+
+
+import com.example.core.ui.state.BaseViewState
+import com.example.domain.models.characters.Character
+import com.example.domain.models.location.Location
+import com.example.domain.state.ApiResult
+import com.example.domain.state.FAIL_RESPONSE_FROM_SERVER
+import com.example.domain.usecases.GetLocationDetailUseCase
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class LocationDetailViewModelTest {
+
+    private lateinit var viewModel: LocationDetailViewModel
+    private val getLocationDetailUSeCase: GetLocationDetailUseCase = mockk()
+
+    @Before
+    fun setup() {
+        viewModel = LocationDetailViewModel(
+            ioDispatcher = UnconfinedTestDispatcher(),
+            getLocationDetailUseCase = getLocationDetailUSeCase,
+            locationId = 1
+        )
+    }
+
+    @Test
+    fun `when we get the initial state this should be loading`() {
+        val state = viewModel.state
+        assertThat(state.value).isEqualTo(BaseViewState.Loading)
+    }
+
+    @Test
+    fun `should emit Error state when GetLocationDetailUseCase throws exception`() = runTest {
+        coEvery { getLocationDetailUSeCase.invoke(locationId = any()) } returns ApiResult.Error(msg = FAIL_RESPONSE_FROM_SERVER)
+
+        viewModel.getLocationDetail(1)
+
+        val state = viewModel.state.value
+        assert(state is BaseViewState.Error)
+        assert((state as BaseViewState.Error).message == FAIL_RESPONSE_FROM_SERVER)
+    }
+
+    @Test
+    fun `should emit Content state when GetLocationDetailUseCase case returns success`() = runTest {
+
+        val detail = GetLocationDetailUseCase.LocationDetail(
+            location = Location.mockLocation(),
+            characters = Character.getCharacters(8)
+        )
+
+        coEvery { getLocationDetailUSeCase.invoke(any()) } returns ApiResult.Success(detail)
+
+        viewModel.getLocationDetail(1)
+
+        val state = viewModel.state.value
+        assert(state is BaseViewState.Content)
+        val result = (state as BaseViewState.Content).result
+//        assertEquals(result, detail.toUi())
+    }
+
+
+}
---
+++ settings/src/test/java/com/example/settings/screens/settings/SettingsViewModelTest.kt
@@ -0,0 +1,68 @@
+package com.example.settings.screens.settings
+
+import org.junit.Assert.*
+
+
+import app.cash.turbine.test
+//import com.example.data.preferences.RickAndMortyPreferences
+import com.google.common.truth.Truth
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verify
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class SettingsViewModelTest {
+
+    private lateinit var viewModel: SettingsViewModel
+//    private val rickAndMortyPreferences = mockk<RickAndMortyPreferences>(relaxed = true)
+
+    @Before
+    fun setup() {
+//        every { rickAndMortyPreferences.getIsNightMode() } returns true
+//        viewModel = SettingsViewModel(rickAndMortyPreferences)
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+    }
+
+    @After
+    fun cleanup() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `when OnToggle event is call  with true param check that state isEnable = true `() =
+        runTest {
+            val viewModel = spyk(objToCopy = viewModel, recordPrivateCalls = true)
+            viewModel.handleEvents(SettingsEvents.OnToggle(isEnable = true))
+            val uiState = viewModel.uiState
+            Truth.assertThat(uiState.value.isNightModeEnable).isTrue()
+            verify(exactly = 1) { viewModel["changeTheme"].invoke(true) }
+        }
+
+    @Test
+    fun `when OnToggle event is call  with false param check that state isEnable = false `() =
+        runTest {
+            viewModel.handleEvents(SettingsEvents.OnToggle(isEnable = false))
+            val uiState = viewModel.uiState
+            com.google.common.truth.Truth.assertThat(uiState.value.isNightModeEnable).isFalse()
+        }
+
+    @Test
+    fun `when init check that initial state of isEnable take the correct value from preferences `() =
+        runTest {
+            viewModel.uiState.test {
+               Truth.assertThat(awaitItem().isNightModeEnable).isTrue()
+            }
+        }
+
+}
